d8d08d74424128843eafb60670aebba22d6945a4
simso <sohyun.sim27@gmail.com>
2023-03-28 11:06:18 +0900

    convert to jp

--- process_vecs.py
+++ process_vecs.py
@@ -2,40 +2,55 @@ import pickle
 import sqlite3
 import sys
 from typing import Set
+import unicodedata
+import re
+from tqdm import tqdm
 
 import numpy as np
 from numpy import array
 
 
-def valid_guess(s: str) -> bool:
-    if all(c.isalpha() or c in '.-' for c in s):
-        return any(c.isalpha() for c in s)
-    else:
-        return False
+# def valid_guess(s: str) -> bool:
+#     if all(c.isalpha() or c in '.-' for c in s):
+#         return any(c.isalpha() for c in s)
+#     else:
+#         return False
 
 
-def only_normal_letters(word: str, allow_capitalization: bool = False) -> bool:
-    lowers = set(c for c in 'abcdefghijklmnopqrstuvwxyzäöǘß')
-    uppers = set(c for c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜẞ')
-    both = lowers.union(uppers)
-    if allow_capitalization:
-        return all(c in both for c in word)
-    else:
-        return all(c in lowers for c in word)
+# def only_normal_letters(word: str, allow_capitalization: bool = False) -> bool:
+#     lowers = set(c for c in 'abcdefghijklmnopqrstuvwxyzäöǘß')
+#     uppers = set(c for c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜẞ')
+#     both = lowers.union(uppers)
+#     if allow_capitalization:
+#         return all(c in both for c in word)
+#     else:
+#         return all(c in lowers for c in word)
 
+def is_japanese(text) -> bool:
+    return bool(re.match(r'^[\u3040-\u309f\u30a0-\u30ff\u4e00-\u9faf\u4e00-\u9faf]+$', text))
 
-def load_dic(path: str, allow_capitalization: bool = False) -> Set[str]:
+def load_dic(path: str) -> Set[str]:
     rtn = set()
-    with open(path, 'r', encoding='utf-16') as f:
+    with open(path, 'r', encoding="utf-8") as f:
         for line in f.readlines():
-            word = line.strip()
-            if only_normal_letters(word, allow_capitalization):
+            word = line.strip().split(',')[11]
+            word = unicodedata.normalize('NFKC', word)
+            if is_japanese(word):
                 rtn.add(word)
-    # april fools and early guesses
-    extras = ['vereinbarung', 'aha', 'tja', 'ah', 'äh']
-    rtn.update(extras)
+    # # april fools and early guesses
+    # extras = ['vereinbarung', 'aha', 'tja', 'ah', 'äh']
+    # rtn.update(extras)
     return rtn
 
+# def blocks(files, size=65536):
+#     while True:
+#         b = files.read(size)
+#         if not b: break
+#         yield
+
+# def count_lines(filepath):
+#     with open(filepath, "r", encoding="utf-8", error="ignore") as f:
+#         return sum(bl.count("\n") for bl in tqdm(blocks(f), desc="Counting lines", mininterval=1))
 
 if __name__ == '__main__':
     skip_db = len(sys.argv) > 1 and sys.argv[1] == '-s'
@@ -45,26 +60,31 @@ if __name__ == '__main__':
     cursor = connection.cursor()
     cursor.execute("""CREATE TABLE IF NOT EXISTS guesses (word text PRIMARY KEY, vec blob)""")
     print("created table")
-    normal_words = load_dic('data/de.dic', True)
+    normal_words = load_dic('data/lex_3_1.csv')
     print("# words in dictionary:", len(normal_words))
     valid_nearest = []
     valid_nearest_mat = []
     eliminated = 0
-    with open('data/cc.de.300.vec', 'r', encoding='utf-8') as w2v_file:
+    checked_words = set()
+    # total_lines = count_lines('data/cc.ja.300.vec') - 1
+    with open('data/cc.ja.300.vec', 'r', encoding='utf-8') as w2v_file:
         _ = w2v_file.readline()
+        # t = tqdm(total=total_lines, desc='Processing vectors', mininterval=1)
         for n, line in enumerate(w2v_file):
             # careful! some data sets (e.g. dewiki100.txt) have non-breaking spaces, which get split
             # others have trailing spaces (e.g. COW.token.wang2vec), meaning an empty string is included with split(' ')
             words = line.rstrip().split(' ')
             word = words[0]
-            vec = array([float(w1) for w1 in words[1:]])
-            if word in normal_words:
-                valid_nearest.append(word)
-                valid_nearest_mat.append(vec)
-            if valid_guess(word) and not skip_db:
-                cursor.execute("""INSERT INTO guesses values (?, ?)""", (word, pickle.dumps(vec)))
-            else:
+            word = unicodedata.normalize('NFKC', word)
+            if not is_japanese(word) or word in checked_words:
                 eliminated += 1
+            else:
+                vec = array([float(w1) for w1 in words[1:]])
+                if word in normal_words:
+                    valid_nearest.append(word)
+                    valid_nearest_mat.append(vec)
+                cursor.execute("""INSERT INTO guesses values (?, ?)""", (word, pickle.dumps(vec)))
+            checked_words.add(word)
             if n % 100000 == 0:
                 print(f"processed {n} (+1) lines")
                 connection.commit()
--- readme.md
+++ readme.md
@@ -13,10 +13,10 @@ pip install -r requirements.txt
 Download Word2Vec and dictionary data:
 ```bash
 cd data
-wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.de.300.vec.gz
-gzip -d cc.de.300.vec.gz
-wget https://www.winedt.org/dict/German.zip
-unzip German.zip
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.ja.300.vec.gz
+gzip -d cc.ja.300.vec.gz
+wget https://clrd.ninjal.ac.jp/unidic_archive/cwj/3.1.0/unidic-cwj-3.1.0.zip
+unzip -p unidic-cwj-3.1.0.zip unidic-cwj-3.1.0/lex_3_1.csv > lex.csv
 ```
 
 save word2vec in db
